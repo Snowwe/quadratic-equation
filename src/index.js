19ad85a0ee4ac32a2ca12d519051427cf2df1f7d
Snezhana <snowwe25@rambler.ru>
2018-09-26 20:30:54 +0300

    version_1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+    let arr = equation.split(' * ');
+    let a = arr[0];
+    let temp = arr[1].split(' ');
+    let b = temp[1] + temp[2];
+    temp = arr[2].split(' ');
+    let c = temp[1] + temp[2];
+    let discriminant = b * b - 4 * a * c;
+    discriminant = Math.round(Math.sqrt(discriminant));
+    let result = [];
+    result.push((-b - discriminant) / (2 * a));
+    result.push((-b + discriminant) / (2 * a));
+    result.sort(function (x, y) {
+        return x - y;
+    });
+    return result;
+};
